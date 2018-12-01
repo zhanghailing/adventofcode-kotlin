dd72e9b800b89db95d170e5db3d6fd619faac6ce
zhanghailing <tkgs.zhang.hailing@gmail.com>
2018-12-02 05:31:13 +0800

    test day1

--- src/test/DayOneTest.kt
+++ src/test/DayOneTest.kt
@@ -9,31 +9,29 @@ internal class DayOneTest {
   fun getFinalFrequencyTest1() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test1.txt"
     var expected = 0
-    var dayOne  = DayOne(fileName)
-    var actual = dayOne.getFinalFrequency()
-    assertEquals(expected, actual)
+    testGetFinalFrequency(fileName, expected)
   }
   @Test
   fun getFinalFrequencyTest2() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test2.txt"
     var expected =4
-    var dayOne  = DayOne(fileName)
-    var actual = dayOne.getFinalFrequency()
-    assertEquals(expected, actual)
+    testGetFinalFrequency(fileName, expected)
   }
   @Test
   fun getFinalFrequencyTest3() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test3.txt"
     var expected =4
-    var dayOne  = DayOne(fileName)
-    var actual = dayOne.getFinalFrequency()
-    assertEquals(expected, actual)
+    testGetFinalFrequency(fileName, expected)
   }
   @Test
   fun getFinalFrequencyTest4() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test4.txt"
     var expected =1
-    var dayOne  = DayOne(fileName)
+    testGetFinalFrequency(fileName, expected)
+  }
+
+  private fun testGetFinalFrequency(fileName: String, expected: Int) {
+    var dayOne = DayOne(fileName)
     var actual = dayOne.getFinalFrequency()
     assertEquals(expected, actual)
   }
@@ -42,31 +40,29 @@ internal class DayOneTest {
   fun getFirstRepeatedFrequencyTest1() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test1.txt"
     var expected =0
-    var dayOne  = DayOne(fileName)
-    var actual = dayOne.getFirstRepeatedFrequency()
-    assertEquals(expected, actual)
+    testGetFirstRep(fileName, expected)
   }
   @Test
   fun getFirstRepeatedFrequencyTest2() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test2.txt"
     var expected =10
-    var dayOne  = DayOne(fileName)
-    var actual = dayOne.getFirstRepeatedFrequency()
-    assertEquals(expected, actual)
+    testGetFirstRep(fileName, expected)
   }
   @Test
   fun getFirstRepeatedFrequencyTest3() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test3.txt"
     var expected =5
-    var dayOne  = DayOne(fileName)
-    var actual = dayOne.getFirstRepeatedFrequency()
-    assertEquals(expected, actual)
+    testGetFirstRep(fileName, expected)
   }
   @Test
   fun getFirstRepeatedFrequencyTest4() {
     var fileName ="C:\\Users\\Hailing\\Desktop\\kotlin_12_01\\input\\day1\\test4.txt"
     var expected =14
-    var dayOne  = DayOne(fileName)
+    testGetFirstRep(fileName, expected)
+  }
+
+  private fun testGetFirstRep(fileName: String, expected: Int) {
+    var dayOne = DayOne(fileName)
     var actual = dayOne.getFirstRepeatedFrequency()
     assertEquals(expected, actual)
   }
